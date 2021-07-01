c1558c320ae1e0ccc4efa4132642b47c8a79c4b0
Kctron <karansinghbisht280@gmail.com>
2021-07-01 12:33:08 +0530

    reuploding

---
+++ DAY_05/index.js
@@ -0,0 +1,30 @@
+let screen= document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenvalue = '';
+for(item of buttons){
+    item.addEventListener('click', (e)=>{
+        buttonText =e.target.innerText;
+        console.log('button text is', buttonText)
+        if(buttonText=='X'){
+            buttonText =  '*';
+            screenvalue += buttonText;
+            screen.value = screenValue
+        }
+        else if(buttonText == 'C'){
+            screenValue = "=";
+            screen.value = screenvalue;
+        
+        }
+        else if (buttonText =='='){
+            screen.value = eval{screenValue};
+           
+        }
+        else{
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+
+
+    })
+}
