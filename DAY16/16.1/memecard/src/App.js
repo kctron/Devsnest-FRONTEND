2d3bd98c8784265b7efdb4688de83dbbeb2ccdaa
James Dai <karansinghbisht280@gmail.com>
2021-07-13 17:18:28 +0530

    meme card

---
+++ DAY16/16.1/memecard/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+    return ( 
+        <div className = "card">
+        <div className = "image" >
+        <img src = "https://images.indianexpress.com/2019/11/dora.jpg"alt = "meme" >
+        </img> 
+        </div > 
+        <h1>Meme card</h1> 
+        <div className = "cardDescription"></div> 
+        </div >
+    );
+}
+
+export default App;
